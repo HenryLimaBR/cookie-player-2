e7651f67b460a34d035194e82ad0bb9f75a3280c
HenryLimaBR <djfilipebr@gmail.com>
2022-01-24 09:34:43 -0300

    Custom App and Document Pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { NextPage } from 'next'
+import Head from 'next/head'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import '../styles/global.scss'
+
+import { PlayerContextProvider } from '../contexts/playerContext'
+import { MainLayout } from '../layouts/MainLayout'
+
+const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Head>
+        <title>Cookie Player 2</title>
+      </Head>
+
+      <PlayerContextProvider>
+        <MainLayout>
+          <Component {...pageProps} />
+        </MainLayout>
+      </PlayerContextProvider>
+    </>
+  )
 }
 
 export default MyApp
