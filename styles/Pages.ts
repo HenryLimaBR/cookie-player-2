6d994c8fd01d8cc0e9ee15dbfc3b5c3432139b40
HenryLimaBR <djfilipebr@gmail.com>
2022-01-24 09:36:30 -0300

    Added Global Styling

---
+++ styles/Pages.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+import { theme } from '../themes/default'
+
+export const PageWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  color: ${theme.colors.fg5};
+`
