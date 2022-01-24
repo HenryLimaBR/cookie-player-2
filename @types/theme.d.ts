2c4bc182e3c68e02b72ca040e46b0ad7fd390195
HenryLimaBR <djfilipebr@gmail.com>
2022-01-24 09:32:34 -0300

    Theme Typings

---
+++ @types/theme.d.ts
@@ -0,0 +1,16 @@
+export interface Theme {
+  name: string
+
+  colors: {
+    bg1: string
+    bg2: string
+    bg3: string
+    bg4: string
+    bg5: string
+    fg1: string
+    fg2: string
+    fg3: string
+    fg4: string
+    fg5: string
+  }
+}
