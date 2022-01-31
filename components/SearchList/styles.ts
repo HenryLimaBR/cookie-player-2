210930c42ab413980638552060dcac1527aab467
HenryLimaBR <djfilipebr@gmail.com>
2022-01-31 13:20:01 -0300

    Custom Webkit Scrollbar on Searchlist component

--- components/SearchList/styles.ts
+++ components/SearchList/styles.ts
@@ -1,10 +1,31 @@
 import styled from 'styled-components'
+import { theme } from '../../themes/default'
 
 export const SearchListWrapper = styled.div`
   width: calc(100% - 16px);
   height: calc(100% - 16px);
 
   overflow-y: auto;
+
+  &::-webkit-scrollbar {
+    appearance: none;
+    width: 8px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background-color: ${theme.colors.bg3};
+    border-radius: 4px;
+  }
+
+  &::-webkit-scrollbar-thumb {	
+    height: 10%;
+    background-color: ${theme.colors.fg1};
+    border-radius: 4px;
+
+    &:hover {
+      background-color: ${theme.colors.fg2};
+    }
+  }
 `
 
 export const SearchListComponent = styled.ul`
