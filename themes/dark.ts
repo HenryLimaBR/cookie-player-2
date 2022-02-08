d081035d6496959c635aba54b977bcc40b535dcf
HenryLimaBR <djfilipebr@gmail.com>
2022-02-08 13:56:31 -0300

    Added Dark Theme

---
+++ themes/dark.ts
@@ -0,0 +1,18 @@
+import type { Theme } from '../@types/theme'
+
+export const darkTheme: Theme = {
+  name: 'light',
+  colors: {
+    bg1: "#000000",
+    bg2: "#111111",
+    bg3: "#222222",
+    bg4: "#333333",
+    bg5: "#666666",
+    fg1: "#888888",
+    fg2: "#aaaaaa",
+    fg3: "#cccccc",
+    fg4: "#dddddd",
+    fg5: "#eeeeee",
+    l1: "#ffffff",
+  }
+}
