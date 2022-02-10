17090d7f08629ff30885f7e977d6d309e616efdc
HenryLimaBR <djfilipebr@gmail.com>
2022-02-10 03:39:22 -0300

    Added Volume State

--- contexts/playerContext.tsx
+++ contexts/playerContext.tsx
@@ -1,4 +1,5 @@
-import React, { createContext, useEffect, useCallback } from 'react'
+import React, { createContext, useEffect, useCallback, useState } from 'react'
+import { State } from '../@types/generic'
 
 type PlayerContextProps = {
   player: HTMLAudioElement
@@ -6,6 +7,7 @@ type PlayerContextProps = {
   isPlaying: boolean
   duration: number
   play: () => Promise<void>
+  volumeState: State<number, (volume: number) => void>
 }
 
 export const playerContext = createContext({} as PlayerContextProps)
@@ -15,10 +17,11 @@ type Props = {
 }
 
 export const PlayerContextProvider: React.FC<Props> = (props) => {
-  const [player, setPlayer] = React.useState<HTMLAudioElement>({} as HTMLAudioElement)
-  const [currentTime, setCurrentTime] = React.useState(0)
-  const [isPlaying, setIsPlaying] = React.useState(false)
-  const [duration, setDuration] = React.useState(0)
+  const [player, setPlayer] = useState<HTMLAudioElement>({} as HTMLAudioElement)
+  const [currentTime, setCurrentTime] = useState(0)
+  const [isPlaying, setIsPlaying] = useState(false)
+  const [duration, setDuration] = useState(0)
+  const [volume, setStateVolume] = useState(50)
 
   useEffect(() => {
     if (!(player instanceof HTMLAudioElement)) {
@@ -33,6 +36,9 @@ export const PlayerContextProvider: React.FC<Props> = (props) => {
       const durationCallback = () => {
         setDuration(player.duration)
       }
+      const volumeCallback = () => {
+        setStateVolume(Math.floor(player.volume * 100))
+      }
 
       player.addEventListener('timeupdate', currentTimeCallback)
 
@@ -42,6 +48,8 @@ export const PlayerContextProvider: React.FC<Props> = (props) => {
 
       player.addEventListener('canplay', durationCallback)
 
+      player.addEventListener('volumechange', volumeCallback)
+
       return () => {
         player.removeEventListener('timeupdate', currentTimeCallback)
 
@@ -50,12 +58,18 @@ export const PlayerContextProvider: React.FC<Props> = (props) => {
         player.removeEventListener('ended', isPlayingCallback)
 
         player.removeEventListener('canplay', durationCallback)
+
+        player.removeEventListener('volumechange', volumeCallback)
       }
     }
+  }, [player, setStateVolume])
+
+  const play = useCallback(async () => {
+    player.play()
   }, [player])
 
-  const play = useCallback(() => {
-    return player.play()
+  const setVolume = useCallback((vol: number) => {
+    player.volume = vol / 100
   }, [player])
 
   return (
@@ -65,7 +79,8 @@ export const PlayerContextProvider: React.FC<Props> = (props) => {
         currentTime,
         isPlaying,
         duration,
-        play
+        play,
+        volumeState: [volume, setVolume]
       }}
     >
       {props.children}
