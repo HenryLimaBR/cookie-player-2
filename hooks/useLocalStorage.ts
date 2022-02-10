72736f88d2899192b99e06898ef50be26e7ec063
HenryLimaBR <djfilipebr@gmail.com>
2022-02-10 03:38:58 -0300

    Use LocalStorage Hook Error Handler

--- hooks/useLocalStorage.ts
+++ hooks/useLocalStorage.ts
@@ -1,15 +1,24 @@
 import React, { useState } from 'react'
 
-export function useLocalStorage<T = string>(key: string, initialValue: T) {
+export function useLocalStorage<T>(key: string, initialValue: T): [T, React.Dispatch<React.SetStateAction<T>>] {
   const [storedValue, setStoredValue] = useState<T>(() => {
-    const item = window.localStorage.getItem(key)
-    return item ? JSON.parse(item) : initialValue
+    try {
+      const item = window.localStorage.getItem(key)
+      return item ? JSON.parse(item) : initialValue
+    } catch (err) {
+      return initialValue
+    }
   })
 
   const setValue: React.Dispatch<React.SetStateAction<T>> = (value) => {
     const valueToStore = value instanceof Function ? value(storedValue) : value
-    setStoredValue(valueToStore)
-    window.localStorage.setItem(key, JSON.stringify(valueToStore))
+    try {
+      window.localStorage.setItem(key, JSON.stringify(valueToStore))
+    } catch(err) {
+      setStoredValue(valueToStore)
+    } finally {
+      setStoredValue(valueToStore)
+    }
   }
 
   return [storedValue, setValue]
