75460cbbab468a1de6c02a7fae7bdbbf8d92dc55
HenryLimaBR <djfilipebr@gmail.com>
2022-02-13 00:33:15 -0300

    Green Leaf Theme Adjusts

--- themes/leaf.ts
+++ themes/leaf.ts
@@ -3,16 +3,16 @@ import type { Theme } from '../@types/theme'
 export const leafTheme: Theme = {
   name: 'leaf',
   colors: {
-    bg1: "#121818",
-    bg2: "#172220",
-    bg3: "#1c3028",
-    bg4: "#244332",
+    bg1: "#122018",
+    bg2: "#172820",
+    bg3: "#1c3828",
+    bg4: "#245032",
     bg5: "#335b3f",
     fg1: "#4d7b54",
-    fg2: "#5d8764",
-    fg3: "#6c9272",
-    fg4: "#799c7f",
-    fg5: "#85a58b",
-    l1: "#c5e5c5",
+    fg2: "#5d9064",
+    fg3: "#6ca072",
+    fg4: "#79bc7f",
+    fg5: "#85ce8b",
+    l1: "#c5f5c5",
   }
 }
